8283d59963ddef23212c785c591824262f88946c
Vance Lucas <vance@vancelucas.com>
2016-04-04 18:33:50 -0500

    Add deploy script to deploy to gh-pages

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+GIT_REPO_URL=$(git config --get remote.origin.url)
+
+# Build the JS bundle
+npm run build
+
+# Copy everything in public/ and deploy it to gh-pages
+mkdir .deploy
+cp -R ./public/* .deploy
+cd .deploy
+git init .
+git remote add github $GIT_REPO_URL
+git checkout -b gh-pages
+git add .
+git commit -am "Static site deploy"
+git push github gh-pages --force
+cd ..
+rm -rf .deploy
